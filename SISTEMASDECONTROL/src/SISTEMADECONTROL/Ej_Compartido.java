fa95f1869199653d75cd020813b414b0529a56e5
Lorraine2507 <lorenanav14@gmail.com>
2023-12-12 13:21:24 +0100

    primer commit de Lorena_in_progress (Circunferencia)

--- SISTEMASDECONTROL/src/SISTEMADECONTROL/Ej_Compartido.java
+++ SISTEMASDECONTROL/src/SISTEMADECONTROL/Ej_Compartido.java
@@ -8,17 +8,39 @@ public class Ej_Compartido {
 		// TODO Auto-generated method stub
 
 		Scanner in = new Scanner(System.in);
+		// CASO TRIÁNGULO
 		double base = 0;
 		double altura = 0;
+		System.out.println("CASO TRIÁNGULO");
 		System.out.println("Introduce la altura");
 		altura = in.nextDouble();
 		System.out.println("Introduce la base: ");
 		base = in.nextDouble();
 		System.out.println("El área del triángulo es: " + calcularAreaTriangulo(base, altura));
+		System.out.println("\n");
+
+		// CASO CIRCUNFERENCIA
+		System.out.println("CASO CIRCUNFERENCIA");
+		double radio = 0;
+		System.out.println("Introduce el radio: ");
+		radio = in.nextDouble();
+		// Calcular el área y el perímetro
+		System.out.println("El área de la circunferencia es: " + calcularAreaCircunferenciaL(radio));
+		System.out.println("El perímetro de la circunferencia es: " + calcularPerimetroCircunferenciaL(radio));
 
 	}
 
 	public static double calcularAreaTriangulo(double base, double altura) {
 		return (base * altura) / 2;
+	}
+
+	public static double calcularAreaCircunferenciaL(double radio) {
+		return 3.14 * radio * radio;
+
+	}
+
+	public static double calcularPerimetroCircunferenciaL(double radio) {
+		return 2 * 3.14 * radio;
+
 	}
 }
